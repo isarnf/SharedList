fc3dd04b086e3e21015b8cdbe8d8c7e5861fd38d
isarnf <isarnf@gmail.com>
2023-06-18 11:15:06 -0300

    feat: created layout for activity_main and a new xml file for each task that will be displayed

--- app/src/main/java/br/edu/scl/ifsp/ads/sharedlist/view/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/sharedlist/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.edu.scl.ifsp.ads.sharedlist
+package br.edu.scl.ifsp.ads.sharedlist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.edu.scl.ifsp.ads.sharedlist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
